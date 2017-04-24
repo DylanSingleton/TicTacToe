a00890fe5e147ab36a6888f262944cd343cce78a
DylanSingleton <T3aBags@hotmail.co.uk>
2017-04-24 23:31:16 +0100

    Add files via upload

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,6 +1,7 @@
 import random
 
 interface = range(0,10)
+totalGames = 0
 
 def menu():
     print("Welcome to TicTacToe. Created By Dylan Singleton\n\nStart Game\nCheck Score\nExit\n ")
@@ -14,6 +15,7 @@ def menu():
         else:
             playerTwoInput()
     elif option.upper() == "check score".upper():
+        updateGames(totalGames)
         fileName = "Wins.txt"
         file = open(fileName)
         score = file.read()
@@ -23,14 +25,7 @@ def menu():
         raw_input("Press ENTER to return to the MENU. ")
         menu()
     elif option.upper() == "exit".upper():
-        choice = raw_input("Are you sure you want to EXIT the game? Yes/No:  ")
-        if choice.upper() == "yes".upper():
-            quit()
-        elif choice.upper() == "no".upper():
-            menu()
-        else:
-            print("Please pick a valid option. ")
-            menu()
+            onExit()
     else:
         print("Please pick a valid option. ")
         menu()
@@ -77,53 +72,17 @@ def playerTwoInput():
             playerTwoInput()
 
 def checkBoard():
-    #Horizontal x
-    if interface[1] == 'X' and interface[2] == 'X' and interface[3] == 'X':
-        xWin()
-    elif interface[4] == 'X' and interface[5] == 'X' and interface[6] == 'X':
-        xWin()
-    elif interface[7] == 'X' and interface[8] == 'X' and interface[9] == 'X':
-        xWin()
-
-    #Vertical x
-    elif interface[1] == 'X' and interface[4] == 'X' and interface[7] == 'X':
-        xWin()
-    elif interface[2] == 'X' and interface[5] == 'X' and interface[8] == 'X':
-        xWin()
-    elif interface[3] == 'X' and interface[6] == 'X' and interface[9] == 'X':
-        xWin()
-
-    #Diagnal x
-    elif interface[1] == 'X' and interface[5] == 'x' and interface[9] == 'X':
-        xWin()
-    elif interface[7] == 'X' and interface[5] == 'x' and interface[3] == 'X':
-        xWin()
-
-    #Horizontal o
-    elif interface[1] == 'O' and interface[2] == 'O' and interface[3] == 'O':
-        oWin()
-    elif interface[4] == 'O' and interface[5] == 'O' and interface[6] == 'O':
-        oWin()
-    elif interface[7] == 'O' and interface[8] == 'O' and interface[9] == 'O':
-        oWin()
-
-    #Vertical o
-    elif interface[1] == 'O' and interface[4] == 'O' and interface[7] == 'O':
-        oWin()
-    elif interface[2] == 'O' and interface[5] == 'O' and interface[8] == 'O':
-        oWin()
-    elif interface[3] == 'O' and interface[6] == 'O' and interface[9] == 'O':
-        oWin()
-
-    #Diagnal o
-    elif interface[1] == 'O' and interface[5] == 'O' and interface[9] == 'o':
-        oWin()
-    elif interface[7] == 'O' and interface[5] == 'O' and interface[3] == 'o':
-        oWin()
-
-    #Draw
-    elif interface[1] != 1 and interface[2] != 2 and interface[3] != 3 and interface[4] != 4 and interface[5] != 5 and interface[6] != 6 and interface[7] != 7 and interface[8] != 8 and interface[9] != 9:
-        gameDraw()
+    #All possible winning positions
+    winPos = ((1,2,3),(4,5,6),(7,8,9),(1,4,7),(2,5,8),(3,6,9),(1,5,9),(7,5,3))
+    #Replaces a,b,c with (1,2,3) ect...
+    for a,b,c in winPos:
+        #Checks to see is a,b,c = X or Y. If they do execute xWin or oWin. Otherwise check for a draw.
+        if interface[a] and interface[b] and interface[c] == 'X':
+            xWin()
+        elif interface[a] and interface[b] and interface[c] == 'O':
+            oWin()
+        elif interface[1] != 1 and interface[2] != 2 and interface[3] != 3 and interface[4] != 4 and interface[5] != 5 and interface[6] != 6 and interface[7] != 7 and interface[8] != 8 and interface[9] != 9:
+            gameDraw()
 
 def xWin():
     showBoard()
@@ -141,7 +100,12 @@ def oWin():
     file = open(fileName, 'a')
     file.writelines("Player TWO has won! \n")
     file.close()
+
     resetGame()
+
+def updateGames(totalGames):
+    totalGames += 1
+    print "Games Played: ", totalGames
 
 def gameDraw():
     showBoard()
@@ -157,5 +121,14 @@ def resetGame():
     interface = range(0, 10)
     menu()
 
+def onExit():
+    choice = raw_input("Are you sure you want to EXIT the game? Yes/No: ")
+    if choice.upper() == "yes".upper():
+        quit()
+    elif choice.upper() == "no".upper():
+        menu()
+    else:
+        print("Please pick a valid option.")
+        onExit()
 #Runs the menu
 menu()
